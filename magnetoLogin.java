b333ef30ed77481c77dea2d18ca38aa1b99fdbaf
garshriv <45705346+garshriv@users.noreply.github.com>
2019-01-05 13:01:53 +0530

    Add files via upload

---
+++ magnetoLogin.java
@@ -0,0 +1,11 @@
+package magento.com;
+
+public class magnetoLogin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+	}
+
+}
